ab4f1f5fd7460ad420356643ff2a1de36a9a9c2b
httse9 <62588559+httse9@users.noreply.github.com>
2026-01-25 17:53:25 -0700

    merge

--- function_approximation/eigenlearning_dr.py
+++ function_approximation/eigenlearning_dr.py
@@ -218,7 +218,7 @@ if __name__ == "__main__":
     run_name = group_name + f"-{args.seed}"
 
     run = wandb.init(
-        project=f"minigrid-eigen-dr-norm",
+        project=f"testing-minigrid-eigen-dr-norm",
         config=vars(args),
         group=group_name,  
         job_type="train",
